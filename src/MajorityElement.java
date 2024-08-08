26a64898713c10a00142b27ae3e8dc64d82b793a
Kent Facey <55371702+kentfacey@users.noreply.github.com>
2024-08-07 23:10:32 -0400

    MajorityElement

---
+++ src/MajorityElement.java
@@ -0,0 +1,70 @@
+/**
+ * Given an array nums of size n, return the majority element.
+ *
+ * The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: nums = [3,2,3]
+ * Output: 3
+ * Example 2:
+ *
+ * Input: nums = [2,2,1,1,1,2,2]
+ * Output: 2
+ *
+ *
+ * Constraints:
+ *
+ * n == nums.length
+ * 1 <= n <= 5 * 104
+ * -109 <= nums[i] <= 109
+ *
+ *
+ * Follow-up: Could you solve the problem in linear time and in O(1) space?
+ */
+public class MajorityElement {
+
+    // Moore Voting Algorithm
+    public int majorityElement(int[] nums) {
+        int majority = nums[0];
+        int count = 1;
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] == majority) {
+                count++;
+            }
+            else {
+                count--;
+            }
+            if (count == 0) {
+                majority = nums[i];
+                count++;
+            }
+        }
+
+        return majority;
+    }
+
+
+    public int majorityElement2(int[] nums) {
+        // better looking version
+        int count = 0;
+        int candidate = 0;
+
+        for (int num : nums) {
+            if (count == 0) {
+                candidate = num;
+            }
+
+            if (num == candidate) {
+                count++;
+            } else {
+                count--;
+            }
+        }
+
+        return candidate;
+    }
+}
