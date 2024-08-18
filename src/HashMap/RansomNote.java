81aea1d478109aacbc07bcfbad1acf775ea5bf47
Kent Facey <55371702+kentfacey@users.noreply.github.com>
2024-08-18 00:46:22 -0400

    RansomNote.java

---
+++ src/HashMap/RansomNote.java
@@ -0,0 +1,52 @@
+package HashMap;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Given two strings ransomNote and magazine, return true if ransomNote can be constructed by using the letters from magazine and false otherwise.
+ *
+ * Each letter in magazine can only be used once in ransomNote.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: ransomNote = "a", magazine = "b"
+ * Output: false
+ * Example 2:
+ *
+ * Input: ransomNote = "aa", magazine = "ab"
+ * Output: false
+ * Example 3:
+ *
+ * Input: ransomNote = "aa", magazine = "aab"
+ * Output: true
+ *
+ *
+ * Constraints:
+ *
+ * 1 <= ransomNote.length, magazine.length <= 105
+ * ransomNote and magazine consist of lowercase English letters.
+ */
+public class RansomNote {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        Map<Character, Integer> map = new HashMap<>();
+
+        for (char c : magazine.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+
+        for (char c : ransomNote.toCharArray()) {
+            int num = map.getOrDefault(c, 0);
+            if (num == 0) {
+                return false;
+            }
+            else {
+                map.put(c, num - 1);
+            }
+        }
+
+        return true;
+    }
+}
